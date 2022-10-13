9062aba792c78ee5d1c8bcefd709ee8f5a4c8789
“daliashaman” <daliashaman@gmail.com>
2022-10-13 19:11:07 -0400

    JavaScript Refresher Lab

---
+++ JavaScript-refresher-lab/script.js
@@ -0,0 +1,16 @@
+var firstName = "Dalia";
+var lastName = "Shaman";
+
+let fullName = "firstName " + "lastName";
+var population = "672,351";
+let javaScriptIsCool = true;
+
+console.log(fullName);
+console.log(population);
+console.log('population' *3);
+
+for(x= 1; x <= 5; x++)
+console.log(x);
+
+for(y=0; y <=4; y++)
+console.log(y)
